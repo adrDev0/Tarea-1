f6436bbbc54459421e3f368b0d52f0d52b86b9df
adrDev0 <165166065+adrDev0@users.noreply.github.com>
2024-09-18 20:42:23 -0600

    Main Commit

---
+++ src/main/java/com/project/demo/rest/product/ProductRestController.java
@@ -0,0 +1,79 @@
+package com.project.demo.rest.product;
+
+import com.project.demo.logic.entity.category.Category;
+import com.project.demo.logic.entity.category.CategoryRepository;
+import com.project.demo.logic.entity.product.Product;
+import com.project.demo.logic.entity.product.ProductRepository;
+import com.project.demo.logic.entity.product.ProductRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/products")
+public class ProductRestController {
+    @Autowired
+    private ProductRepository productRepository;
+
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+    @GetMapping()
+    public List<Product> getAllProducts() {
+        return productRepository.findAll();
+    }
+
+    @PostMapping("/create")
+    @PreAuthorize("hasAnyRole('SUPER_ADMIN_ROLE')")
+    public ResponseEntity<?> createProduct(@RequestBody ProductRequest productRequest) {
+        Optional<Category> optionalCategory = categoryRepository.findById(productRequest.getCategoryId());
+        if (optionalCategory.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Category not found");
+        }
+
+        Product product = new Product();
+        product.setName(productRequest.getName());
+        product.setDescripcion(productRequest.getDescripcion());
+        product.setPrice(productRequest.getPrice());
+        product.setStock(productRequest.getStock());
+        product.setCategory(optionalCategory.get());
+
+        Product savedProduct = productRepository.save(product);
+        return ResponseEntity.ok(savedProduct);
+    }
+
+    @GetMapping("/{id}")
+    @PreAuthorize("hasAnyRole('SUPER_ADMIN_ROLE')")
+    public Product getProductById(@PathVariable Long id) {
+        return productRepository.findById(id).orElseThrow(RuntimeException::new);
+    }
+
+    @PutMapping("/{id}")
+    @PreAuthorize("hasAnyRole('SUPER_ADMIN_ROLE')")
+    public Product updateProduct(@PathVariable Long id, @RequestBody Product product) {
+        return productRepository.findById(id)
+                .map(existingProduct -> {
+                    existingProduct.setName(product.getName());
+                    existingProduct.setDescripcion(product.getDescripcion());
+                    existingProduct.setPrice(product.getPrice());
+                    existingProduct.setStock(product.getStock());
+                    existingProduct.setCategory(product.getCategory());
+                    return productRepository.save(existingProduct);
+                })
+                .orElseGet(() -> {
+                    product.setId(id);
+                    return productRepository.save(product);
+                });
+    }
+
+    @DeleteMapping("/{id}")
+    @PreAuthorize("hasAnyRole('SUPER_ADMIN_ROLE')")
+    public void deleteProduct(@PathVariable Long id) {
+        productRepository.deleteById(id);
+    }
+}
